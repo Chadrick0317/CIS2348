18d8247a768cf397f4ef3277dfe01036026ae219
Chadrick0317 <70494157+Chadrick0317@users.noreply.github.com>
2021-03-03 18:28:52 -0600

    Add files via upload

---
+++ Homework2/PasswordModifier.py
@@ -0,0 +1,13 @@
+#Chad Johnson 1323504 Password modifier
+word = input()
+
+word = word.replace('i','!') #i becomes !
+word = word.replace('a','@') #a becomes @
+word = word.replace('m','M') #m becomes M
+word = word.replace('B','8') #B becomes 8
+word = word.replace('o','.') #o becomes .
+
+word = word + 'q*s' #appending "q*s"
+
+print(word)
+#Chad Johnson 1323504 Password modifier
