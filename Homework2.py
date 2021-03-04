1873a0b5c71490afc4fa309d41b923a1a46ca1a4
Chadrick0317 <70494157+Chadrick0317@users.noreply.github.com>
2021-03-03 18:13:37 -0600

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -1,4 +1,4 @@
-#Chad Johnson 1323504 Homework Assignment 2
+#Chad Johnson 1323504 Homework Assignment 2 
 
 month_list={ "january":"1","february":"2", "march":"3","april":"4", "may":"5", "june":
     "6","july":"7", "august":"8", "september":"9","october":"10", "november":"11", "december":"12"}
